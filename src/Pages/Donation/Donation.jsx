f930bf3ff6cd3c461a3425766a53f76d524e2f35
Minhajul-alam <alam.minhajul@outlook.com>
2023-09-26 08:43:55 +0600

    Added see more btn in Donation Page and make it fuctional

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -4,6 +4,11 @@ import DonationCard from "./DonationCard/DonationCard";
 const Donation = () => {
     const [donations, setDonetions] = useState([]);
     const [noFound, setNoFound] = useState('');
+
+
+    const [isShow, setIsShow] = useState(false)
+
+
     useEffect(() => {
         const allDonation = JSON.parse(localStorage.getItem("donations"));
         if (allDonation) {
@@ -13,17 +18,25 @@ const Donation = () => {
         }
     }, [])
 
+
     return (
         <div className="my-8">
 
-            {noFound ? <p>{noFound}</p>
-                :
-                <div>
-                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-3">
-                        {donations.map(donation => <DonationCard key={donation.id} donation={donation}></DonationCard>)}
-                    </div>
+            
+
+            {
+                noFound ? <p className="h-[80vh] flex justify-center items-center">{noFound}</p> : <div className="grid grid-cols-1 lg:grid-cols-2 gap-3">
+                    {
+                        isShow ? donations.map(donation => (<DonationCard key={donation.id} donation={donation}></DonationCard>))
+                            : donations.slice(0, 3).map(donation => <DonationCard key={donation.id} donation={donation}></DonationCard>)
+                    }
                 </div>
             }
+            {
+                donations.length > 3 ? <button onClick={() => { setIsShow(true) }} className="btn text-base font-semibold text-white hover:bg-green-700 block mx-auto mt-2 bg-[#009444]">See More</button> : ""
+            }
+
+
         </div>
     );
 };
